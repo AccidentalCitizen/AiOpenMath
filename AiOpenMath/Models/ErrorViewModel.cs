07463d19b11f529dc000346f84c94e33f7932fdd
Citizen <codeaiplings@gmail.com>
2023-07-13 13:04:25 +0200

    Base Commit with Project

---
+++ AiOpenMath/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AiOpenMath.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
